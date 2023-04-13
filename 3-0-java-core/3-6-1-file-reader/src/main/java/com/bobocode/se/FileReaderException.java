e44a0da1cae92c63b18d90b4ea367b3b4c0ecff2
Oleksandr Rolik <rolik.alexx@gmail.com>
2023-04-14 01:57:45 +0300

    implement FileReaders task

---
+++ 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaderException.java
@@ -0,0 +1,11 @@
+package com.bobocode.se;
+
+public class FileReaderException extends RuntimeException {
+    public FileReaderException(String message) {
+        super(message);
+    }
+
+    public FileReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
+++ 3-0-java-core/3-6-1-file-reader/src/main/java/com/bobocode/se/FileReaders.java
@@ -1,6 +1,15 @@
 package com.bobocode.se;
 
-import com.bobocode.util.ExerciseNotCompletedException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
 
 /**
  * {@link FileReaders} provides an API that allow to read whole file into a {@link String} by file name.
@@ -14,6 +23,27 @@ public class FileReaders {
      * @return string that holds whole file content
      */
     public static String readWholeFile(String fileName) {
-        throw new ExerciseNotCompletedException(); //todo
+        Path filePath = createPath(fileName);
+        try (Stream<String> fileLinesStream = openFile(filePath)) {
+            return fileLinesStream.collect(joining("\n"));
+        }
+    }
+
+    private static Path createPath(String filename) {
+        Objects.requireNonNull(filename);
+        URL fileUrl = FileReaders.class.getClassLoader().getResource(filename);
+        try {
+            return Paths.get(fileUrl.toURI());
+        } catch (URISyntaxException e) {
+            throw new FileReaderException("Can't create path ", e);
+        }
+    }
+
+    private static Stream<String> openFile(Path filePath) {
+        try {
+            return Files.lines(filePath);
+        } catch (IOException e) {
+            throw new FileReaderException("Couldn't open file ", e);
+        }
     }
 }
